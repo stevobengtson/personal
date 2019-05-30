dc507412a6b423546fc610e3eb8baad405cdd741
Steven Bengtson <steven.bengtson@gmail.com>
2019-05-30 14:33:00 -0700

    Add main redirect to resume

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/resume', pathMatch: 'full' },
+  { path: '', redirectTo: '/resume', pathMatch: 'full' },
   { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) }
 ];
 
